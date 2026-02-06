7c2b1a057f3c83e578528cba66b5186e1429a87d
mamartam <127197633+mamartam@users.noreply.github.com>
2026-02-06 11:32:55 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,63 +4,9 @@ const bookGenres = document.querySelector("#book-genres");
 const priceFiltering = document.querySelector("#price-filtering");
 const resetAllFilters = document.querySelector(".reset-all-filters");
 
-async function gettingInfoFromExternalSource() {
-  try {
-    const response = await fetch("https://gutendex.com/books/");
-    const data = await response.json();
+//  FUNCTIONS
 
-    const arrayOfData = [];
-    data.results.forEach((element, index, array) => {
-      element.cost = Math.floor(Math.random() * (1000 - 200 + 1)) + 200;
-      arrayOfData.push(element);
-    });
-    console.log(arrayOfData);
-
-    displayingBooksToTheScreen(arrayOfData);
-
-    searchInput.addEventListener("input", (element) => {
-      let inputedValue = element.target.value.toLowerCase().trim();
-      let newArray = arrayOfData.filter((item) => {
-        return item.title.toLowerCase().includes(inputedValue);
-      });
-      displayingBooksToTheScreen(newArray);
-    });
-    bookGenres.addEventListener("change", (element) => {
-      let selectedGenre = element.target.value;
-      console.log(selectedGenre);
-      let newArray = arrayOfData.filter((item) => {
-        return item.subjects[0].toLowerCase().includes(selectedGenre);
-      });
-      if (selectedGenre === "all") {
-        displayingBooksToTheScreen(arrayOfData);
-      } else {
-        displayingBooksToTheScreen(newArray);
-      }
-    });
-
-    priceFiltering.addEventListener("change", (event) => {
-      console.log(event.target.value);
-      let arrayForSorting = [...arrayOfData];
-      if (event.target.value === "from-cheap-to-expencive") {
-        arrayForSorting.sort((a, b) => Number(a.cost) - Number(b.cost));
-        displayingBooksToTheScreen(arrayForSorting);
-      } else if (event.target.value === "from-expencive-to-cheap") {
-        arrayForSorting.sort((a, b) => Number(b.cost) - Number(a.cost));
-        displayingBooksToTheScreen(arrayForSorting);
-      }
-    });
-    resetAllFilters.addEventListener("click", () => {
-      searchInput.value = "";
-      bookGenres.value = "all";
-      priceFiltering.value = "not-sorted-by-price";
-      displayingBooksToTheScreen(arrayOfData);
-    });
-  } catch (error) {
-    console.log("There is some error", error);
-  }
-}
-gettingInfoFromExternalSource();
-
+// Fucction for creating boxes for books and displaying them on the page
 function displayingBooksToTheScreen(array) {
   booksContainers.innerHTML = "";
   array.forEach((book) => {
@@ -93,4 +39,84 @@ function displayingBooksToTheScreen(array) {
   });
 }
 
-function filteringByKeyWord() {}
+// Searching book by key words in its name
+function filteringByKeyWord(val, array) {
+  let inputedValue = val.target.value.toLowerCase().trim();
+  let newArray = array.filter((item) => {
+    return item.title.toLowerCase().includes(inputedValue);
+  });
+  displayingBooksToTheScreen(newArray);
+}
+
+// Filtering books by genre
+function filteringByGenre(val, array) {
+  let selectedGenre = val.target.value;
+  console.log(selectedGenre);
+  let newArray = array.filter((item) => {
+    return item.subjects[0].toLowerCase().includes(selectedGenre);
+  });
+  if (selectedGenre === "all") {
+    displayingBooksToTheScreen(array);
+  } else {
+    displayingBooksToTheScreen(newArray);
+  }
+}
+
+// Filtering books by its cost
+function filteringBooksByItsCost(val, array) {
+  let chosenOption = val.target.value;
+
+  let arrayForSorting = [...array];
+
+  if (chosenOption === "from-cheap-to-expencive") {
+    arrayForSorting.sort((a, b) => Number(a.cost) - Number(b.cost));
+    displayingBooksToTheScreen(arrayForSorting);
+  } else if (chosenOption === "from-expencive-to-cheap") {
+    arrayForSorting.sort((a, b) => Number(b.cost) - Number(a.cost));
+    displayingBooksToTheScreen(arrayForSorting);
+  }
+}
+
+async function gettingInfoFromExternalSource() {
+  try {
+    const response = await fetch("https://gutendex.com/books/");
+    const data = await response.json();
+
+    // Array for data from external source, so we can interact with it
+    const arrayOfData = [];
+
+    // Here we push elements to the array abou and adding new property - cost, since the initial array doesn't have it
+    data.results.forEach((element) => {
+      element.cost = Math.floor(Math.random() * (1000 - 200 + 1)) + 200;
+      arrayOfData.push(element);
+    });
+
+    // Calling function for displpaying all data on the page
+    displayingBooksToTheScreen(arrayOfData);
+
+    // Searching book by key words in its name
+    searchInput.addEventListener("input", (element) => {
+      filteringByKeyWord(element, arrayOfData);
+    });
+
+    // Filtering books by genre
+    bookGenres.addEventListener("change", (element) => {
+      filteringByGenre(element, arrayOfData);
+    });
+
+    // Filtering books by its cost
+    priceFiltering.addEventListener("change", (event) => {
+      filteringBooksByItsCost(event, arrayOfData);
+    });
+    // Buttom to clear all filters and got back to the starting position where user can see all books
+    resetAllFilters.addEventListener("click", () => {
+      searchInput.value = "";
+      bookGenres.value = "all";
+      priceFiltering.value = "not-sorted-by-price";
+      displayingBooksToTheScreen(arrayOfData);
+    });
+  } catch (error) {
+    console.log("There is some error", error);
+  }
+}
+gettingInfoFromExternalSource();
